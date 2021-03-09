3a2737299ec6864eb8bff437a044b81cf8e9cf07
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-03-09 23:19:08 +0000

    Implemented basic version of player's hand on the game board

--- content/js/modules/gameVue.js
+++ content/js/modules/gameVue.js
@@ -19,8 +19,26 @@ const makeGameInfoVue = function() {
 const makePlayerHandVue = function() {
     const playerHandVue = new Vue({
         el: "#player-hand",
-        data: {msg: 'msg from playerHandVue'},
-        methods: {}
+        data: {
+            msg: 'msg from playerHandVue',
+            hand: []
+        },
+        methods: {
+            getHand: function() {
+                // get cards from api
+                let invalidCharCodes = [127148, 127151, 127152, 127164, 127167, 127168, 127180, 127183, 127184, 127196];
+                let cards = Array.from(Array(62).keys()).map(el => el + 127137).filter(el => !invalidCharCodes.includes(el));
+                let hand = []
+                for (let c of cards) {
+                    hand.push({card: '&#'+c+';', color: ((c <= 127150) || (c >= 127185)) ? "black" : "darkred"})
+                }
+                // todo don't slice => just for showcase
+                this.hand = hand.slice(7, 17);
+            }
+        },
+        created: function() {
+            this.getHand();
+        }
     })
 }
 
