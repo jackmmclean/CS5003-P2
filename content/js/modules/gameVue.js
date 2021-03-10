3c3507af739db663d5d6ef32a4965872c5dd7a1d
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-03-10 00:08:52 +0000

    Implemented basic open deck on the game board

--- content/js/modules/gameVue.js
+++ content/js/modules/gameVue.js
@@ -25,14 +25,13 @@ const makePlayerHandVue = function() {
         methods: {
             getHand: function() {
                 // get cards from api
+
+                // just as long as we don't have the real data
                 let invalidCharCodes = [127148, 127151, 127152, 127164, 127167, 127168, 127180, 127183, 127184, 127196];
                 let cards = Array.from(Array(62).keys()).map(el => el + 127137).filter(el => !invalidCharCodes.includes(el));
-                let hand = []
-                for (let c of cards) {
-                    hand.push({card: '&#'+c+';', color: ((c <= 127150) || (c >= 127185)) ? "black" : "darkred"})
-                }
+
                 // todo don't slice => just for showcase
-                this.hand = hand.slice(7, 17);
+                this.hand = transformCards(cards.slice(7, 17));
             }
         },
         created: function() {
@@ -45,7 +44,7 @@ const makeClosedDeckVue = function() {
     const closedDeckVue = new Vue({
         el: "#closed-deck",
         data: {
-            backOfCard: {card: "&#127136", color: "royalblue"}
+            backOfCard: {card: "&#127136", color: "#0d47a1"}
         },
         methods: {}
     })
@@ -54,9 +53,33 @@ const makeClosedDeckVue = function() {
 const makeOpenDeckVue = function() {
     const openDeckVue = new Vue({
         el: "#open-deck",
-        data: {msg: 'msg from openDeckVue'},
-        methods: {}
+        data: {
+            openDeckCards: []
+        },
+        methods: {
+            getOpenDeckCards: function() {
+                // get open deck cards from cards
+
+                // just as long as we don't have the real data
+                let invalidCharCodes = [127148, 127151, 127152, 127164, 127167, 127168, 127180, 127183, 127184, 127196];
+                let cards = Array.from(Array(62).keys()).map(el => el + 127137).filter(el => !invalidCharCodes.includes(el));
+
+                // todo don't slice => just for showcase
+                this.openDeckCards = transformCards(cards.slice(37, 41));
+            }
+        },
+        created: function() {
+            this.getOpenDeckCards();
+        }
     })
+}
+
+const transformCards = function(numericCards) {
+    let cards = []
+    for (let c of numericCards) {
+        cards.push({card: '&#'+c+';', color: ((c <= 127150) || (c >= 127185)) ? "black" : "darkred"})
+    }
+    return cards;
 }
 
 export const makeGame = function() {
