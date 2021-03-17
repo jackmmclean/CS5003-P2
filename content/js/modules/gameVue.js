65b8a89796734c1d0190299405229a6b3d411723
Jack McLean <jm461@st-andrews.ac.uk>
2021-03-17 10:30:58 +0000

    added round to game info

--- content/js/modules/gameVue.js
+++ content/js/modules/gameVue.js
@@ -390,6 +390,7 @@ const sharedGameInfo = Vue.observable({
 		Username: "",
 		Time: "some time",
 		Players: 0,
+		Round: 1
 	}
 });
 
@@ -459,6 +460,7 @@ const startInterval = () => {
 				sharedGameInfo.generalInfo.GameID = json.gameId;
 				sharedGameInfo.generalInfo.Username = json.niceUsername;
 				sharedGameInfo.generalInfo.Players = json.numPlayers;
+				sharedGameInfo.generalInfo.Round = json.round;
 			}).catch(err => console.log('Could not get stats.', err))
 
 			// only once the game is started
